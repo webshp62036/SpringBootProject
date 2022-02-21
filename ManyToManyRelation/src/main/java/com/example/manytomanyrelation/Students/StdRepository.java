7ce117f234c777618d966641a225316c1c9a9ebf
webshp62036 <99970677+webshp62036@users.noreply.github.com>
2022-02-21 18:47:42 +0530

    Revert " spring boot project"

--- ManyToManyRelation/src/main/java/com/example/manytomanyrelation/Students/StdRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.manytomanyrelation.Students;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@EnableJpaRepositories
-public interface StdRepository extends JpaRepository<StdEntity,Integer> {
-
-}
--- ManyToManyRelation/src/main/java/com/example/manytomanyrelation/department/DepartmentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.manytomanyrelation.department;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<DepartmentEntity,Integer> {
-}
